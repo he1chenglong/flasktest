50f9fb2450a9afacaa72d8e5405d243a8b46ee16
he1chenglong <he1chenglong@163.com>
2016-08-24 10:03:04 +0800

    可以 轮询

--- code/01flaskapp/flaskapp.py
+++ code/01flaskapp/flaskapp.py
@@ -8,13 +8,14 @@
     :copyright: (c) 2015 by Armin Ronacher.
     :license: BSD, see LICENSE for more details.
 """
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request,flash,json
 import  threading
+import  thread
 import random
 import time
 
 app = Flask(__name__)
-
+app.secret_key = '123'
 
 @app.route('/_add_numbers')
 def add_numbers():
@@ -22,21 +23,45 @@ def add_numbers():
     """Add two numbers server side, ridiculous but well..."""
     a = request.args.get('a', 0, type=int)
     b = request.args.get('b', 0, type=int)
-    return jsonify(result=a + b)
+    # return jsonify(result=a + b)
+    randomnum=random.randint(1,100)
+    return  jsonify(result=randomnum)
+
 
 
 @app.route('/')
 def index():
+    flash("hello jikexueyuan")
+    # thread.start_new_thread(randomthread,(1,1))
     return render_template('index.html')
 
-def randomthread():
+@app.route("/", methods=['POST'])
+def get_data():
+    print 'call get_data'
+    data = json.loads(request.form.get('data'))
+    ss = data['value']
+    print str(ss)
+    return str(ss)
+
+@app.route("/polling",methods=['POST'])
+def polldata():
+    print 'call polldata...'
+    data = request.form.get('data')
+    # ss = data['value']
+    # print str(ss)
+    randomnum=1
+    return  jsonify(result=randomnum)
+
+def randomthread(no, interval):
     # time.sleep(5)
     print 'call randomthread'
+    time.sleep(interval)
     randomnum=random.randint(1,100)
     # return  jsonify(randomnum)
 
 if __name__ == '__main__':
-    t=threading.Thread(target=randomthread)
-    t.start()
-    t.join()
+    # t=threading.Thread(target=randomthread)
+    # t.start()
+    # # t.join()
+
     app.run(threaded=True)
